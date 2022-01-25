6b7ea7a564d69a7e28a4a99dac50ad96e066eebe
pukar neupane <pukar@HO-IT-PUKAR.iginepal.com>
2022-01-25 13:25:37 +0545

    high score implementation

--- script.js
+++ script.js
@@ -14,6 +14,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -31,6 +32,12 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
     //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
